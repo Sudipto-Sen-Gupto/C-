c57c46aee24dca496b459d70df1711a4888de655
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-07-04 11:01:19 -0500

    Sum of two num from user input

---
+++ userValue.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,sum;
+    cout<<"Enter two value=";
+    cin>>a>>b;
+    cout<<"First num is="<<a<<endl<<"Second number is="<<b<<endl;
+    sum=a+b;
+   
+    cout<<"The sum of a and b="<<sum;
+
+}
