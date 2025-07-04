274696e5bfb0611aa73d7cc5459b57b629244529
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-07-04 09:01:42 -0500

    first commit

---
+++ Escape-sequence.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    cout<< "Gaurav Sen Gupto"<< endl<<"Kushtia"<< endl<<"01304788720";
+
+    
+}
---
+++ data_type.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    float a=20.4,b=30.5;
+    char ch='a';
+    cout<<"number1="<<a<<endl<<"number2="<<b<<endl<<"Character is="<<ch;
+    return 0;
+}
