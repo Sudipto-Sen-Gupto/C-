cc0b1b2795b6715415494a26a2d5c16a3fed7608
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-07-05 11:39:30 -0500

    how many number we can give after decimal

---
+++ outputformatting.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    float a,b,sum,subs,mult,div,remainder;
+    cout<<"Enter the value of a and b=";
+    cin>>a>>b;
+    cout<<showpoint;
+    cout<<fixed;
+     cout<<setprecision(10);
+    sum=a+b;
+    cout<<"The sum of a and b="<<sum;
+     
+    subs=a-b;
+    cout<<endl<<"The substraction of a and b="<<subs<<endl;
+     
+    mult=a*b;
+    cout<<"The multiplication of a and b"<<mult<<endl;
+
+    div=a/b;
+    cout<<"the division of a and b="<<div<<endl;
+
+
+   
+
+
+}
