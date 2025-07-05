73f46025e4cb7ff25cacfc55c8398f89c3de5782
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-07-05 03:46:50 -0500

    new file added

---
+++ substraction.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std; 
+int main(){
+    int a,b,subs;
+      cout<<"Enter the value a and b=";
+    cin>>a>>b;
+    subs=a-b;
+    cout<<"The result of substraction="<<subs;
+    return 0;
+}
